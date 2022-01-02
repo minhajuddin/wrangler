2726fa69de67482e04a28c5132cd2201a4e02609
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-01-02 00:31:23 -0500

    init

--- lib/wrangler/tcp.ex
+++ lib/wrangler/tcp.ex
@@ -1,19 +1,49 @@
 defmodule Wrangler.Tcp do
   defmodule Server do
-    use GenServer
     require Logger
 
-    @doc false
-    def start_link(opts) do
-      GenServer.start_link(__MODULE__, opts)
+    def start do
+      Task.async(fn -> init(SimpleWire) end)
     end
 
-    @impl true
-    def init(opts) do
-      {:ok, sock} = :gen_tcp.listen(0, [:binary, {:active, :once}])
-      {:ok, port} = :inet.port(sock)
-      Logger.info(log_code: "tcp_server_started", port: port)
-      {:ok, Map.merge(opts, %{sock: sock, port: port})}
+    def init(wire_proto_mod) do
+      {:ok, listener} = :gen_tcp.listen(0, [:binary, {:packet, 4}, {:active, false}])
+      {:ok, port} = :inet.port(listener)
+      Logger.info(log_code: "listener.started", port: port)
+      accept_loop({listener, wire_proto_mod})
+    end
+
+    # TODO: make this parallely accept
+    defp accept_loop({listener, wire_proto_mod}) do
+      {:ok, sock} = :gen_tcp.accept(listener)
+      spawn(fn -> setup_handler(sock, wire_proto_mod) end)
+      accept_loop({listener, wire_proto_mod})
+    end
+
+    defp setup_handler(sock, wire_proto_mod) do
+      {:ok, peername} = :inet.peername(sock)
+      Logger.info(log_code: "tcp.accepted", peername: peername)
+      serve(sock, wire_proto_mod)
+    end
+
+    defp serve(sock, wire_proto_mod) do
+      case :gen_tcp.recv(sock, 0, :infinity) do
+        {:ok, frame} ->
+          handle_frame(frame, wire_proto_mod)
+          serve(sock, wire_proto_mod)
+
+        {:error, :timeout} ->
+          serve(sock, wire_proto_mod)
+
+        {:error, error} ->
+          Logger.error(error_code: "recv.error", error: error)
+          :gen_tcp.close(sock)
+      end
+    end
+
+    defp handle_frame(frame, wire_proto_mod) do
+      {:ok, message} = wire_proto_mod.parse_frame(frame)
+      Logger.info(log_code: "received.packet", frame: frame, message: message)
     end
   end
 
