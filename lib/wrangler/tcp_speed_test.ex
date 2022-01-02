13289c964a18ae0d19468415d44b438b94ddbb55
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2022-01-02 13:24:16 -0500

    wip

--- lib/wrangler/tcp_speed_test.ex
+++ lib/wrangler/tcp_speed_test.ex
@@ -1,2 +1,36 @@
-defmodule TcpSpeedTest do
+defmodule TCPSpeedTest do
+  defmodule Server do
+    def start(port \\ 5555) do
+      {:ok, listener} = :gen_tcp.open(port, [:binary, {:active, false}])
+      accept_loop(listener)
+    end
+
+    def accept_loop(listener) do
+      {:ok, sock} = :gen_tcp.accept(listener)
+      spawn(fn -> serve_sock(sock, conn_id(sock) 0) end)
+      accept_loop(listener)
+    end
+
+    defp conn_id(sock) do
+      {:ok, peername} = :inet.peername(sock)
+      peername
+    end
+
+    defp serve_sock(sock, conn_id, rx_bytes) do
+      Logger.info log_code: "RX_BYTES", rx_bytes: rx_bytes, conn_id: conn_id
+      case :gen_tcp.recv(sock, 0, 1000) do
+        {:ok, data} ->
+          serve_sock(sock, rx_bytes + byte_length(data))
+        {:error, :timeout} ->
+          serve_sock(sock, rx_bytes)
+        {:error, err} ->
+          Logger.error log_code: "sock.error", rx_bytes: rx_bytes, conn_id: conn_id
+      end
+    end
+  end
+
+  defmodule Client do
+    def start(host, port \\ 5555) do
+    end
+  end
 end
