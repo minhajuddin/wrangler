99f16d84747d0c37581b4cc251b60f035da3a9d4
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-01-11 22:24:52 -0500

    Bug fix

--- lib/wrangler/tcp_speed_test.ex
+++ lib/wrangler/tcp_speed_test.ex
@@ -1,13 +1,15 @@
 defmodule TCPSpeedTest do
   defmodule Server do
+    require Logger
+
     def start(port \\ 5555) do
-      {:ok, listener} = :gen_tcp.open(port, [:binary, {:active, false}])
+      {:ok, listener} = :gen_tcp.listen(port, [:binary, {:active, false}])
       accept_loop(listener)
     end
 
     def accept_loop(listener) do
       {:ok, sock} = :gen_tcp.accept(listener)
-      spawn(fn -> serve_sock(sock, conn_id(sock) 0) end)
+      spawn(fn -> serve_sock(sock, conn_id(sock), 0) end)
       accept_loop(listener)
     end
 
@@ -17,14 +19,14 @@ defmodule TCPSpeedTest do
     end
 
     defp serve_sock(sock, conn_id, rx_bytes) do
-      Logger.info log_code: "RX_BYTES", rx_bytes: rx_bytes, conn_id: conn_id
+      # Logger.info log_code: "RX_BYTES", rx_bytes: rx_bytes, conn_id: conn_id
       case :gen_tcp.recv(sock, 0, 1000) do
         {:ok, data} ->
-          serve_sock(sock, rx_bytes + byte_length(data))
+          serve_sock(sock, conn_id, rx_bytes + byte_size(data))
         {:error, :timeout} ->
-          serve_sock(sock, rx_bytes)
+          serve_sock(sock, conn_id, rx_bytes)
         {:error, err} ->
-          Logger.error log_code: "sock.error", rx_bytes: rx_bytes, conn_id: conn_id
+          Logger.error log_code: "sock.error", rx_bytes: rx_bytes, conn_id: conn_id, error: err
       end
     end
   end
